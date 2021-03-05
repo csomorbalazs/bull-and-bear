76a1f9f1fbd9fd798b988aee02341b306eb2126c
angyaloliver <oliver.angyal@gmail.com>
2021-03-05 23:55:03 +0100

    Fix merge conflict

--- src/app/components/mini-games/money-counting-mini-game/money-counting-mini-game.component.ts
+++ src/app/components/mini-games/money-counting-mini-game/money-counting-mini-game.component.ts
@@ -12,12 +12,44 @@ export class MoneyCountingMiniGameComponent implements OnInit, MiniGame {
   MiniGameState = MiniGameState;
   miniGameState: MiniGameState = MiniGameState.GAMEPLAY;
 
+  targetMoney: number;
+
   @Input() timeLimitInSeconds;
   @Output() finished = new EventEmitter<void>();
 
   constructor(private playerInfoService: PlayerInfoService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.targetMoney = this.getRandomNumberEndingWithFiveOrZero();
+  }
+
+  // generates number between 500 and 2000, ending with 5 or 0
+  private getRandomNumberEndingWithFiveOrZero(): number {
+    let max = 400;
+    let min = 100;
+    return (Math.floor(Math.random() * (max - min + 1)) + min) * 5;
+  }
+
+  onCoinClicked(coinValue: number) {
+    let result = this.targetMoney - coinValue;
+    if (result < 0) {
+      this.miniGameLost();
+    } else if (result == 0) {
+      this.miniGameWon();
+    } else {
+      this.targetMoney -= coinValue;
+    }
+  }
+
+  private miniGameLost() {
+    this.playerInfoService.decreaseCurrentHealth();
+    this.miniGameState = MiniGameState.LOST;
+  }
+
+  private miniGameWon() {
+    this.playerInfoService.increaseScoreBy(50);
+    this.miniGameState = MiniGameState.WON;
+  }
 
   onMiniGameWon() {
     this.playerInfoService.increaseScoreBy(50);
@@ -30,8 +62,6 @@ export class MoneyCountingMiniGameComponent implements OnInit, MiniGame {
   }
 
   onTimeIsUp() {
-    this.playerInfoService.decreaseCurrentHealth();
-    this.miniGameState = MiniGameState.LOST;
-    this.finished.emit();
+    this.miniGameLost();
   }
 }
