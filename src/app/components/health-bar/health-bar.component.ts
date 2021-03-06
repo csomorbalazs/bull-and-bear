2d1589a916bb9e14139cdc59bbcc6caed8c50177
nandorlengyel <lengyel.nandi@gmail.com>
2021-03-06 01:32:40 +0100

    Implement player info

--- src/app/components/health-bar/health-bar.component.ts
+++ src/app/components/health-bar/health-bar.component.ts
@@ -8,7 +8,8 @@ import { Component, Input, OnInit } from '@angular/core';
 export class HealthBarComponent implements OnInit {
   @Input() value: number;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void { }
 
