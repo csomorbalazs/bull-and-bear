0328284a7aa4b74ecb14f2a2a35e57cdb4b762d6
csomorbalazs <csomorbp@gmail.com>
2021-03-07 14:19:26 +0100

    Implement multiple lesson texts feature

--- src/app/components/lesson-screen/lesson-screen.component.ts
+++ src/app/components/lesson-screen/lesson-screen.component.ts
@@ -8,12 +8,27 @@ import { Character } from '../../models/Character';
 })
 export class LessonScreenComponent implements OnInit {
   Character = Character;
+  Array = Array;
 
-  @Input() text: string = "";
+  @Input() text: string | string[] = "";
   @Input() character: Character;
   @Output() lessonViewed = new EventEmitter<void>();
+
+  currentTextIndex = 0;
 
   constructor() { }
 
   ngOnInit(): void { }
+
+  onNextPressed(): void {
+    if (Array.isArray(this.text)) {
+      if (this.currentTextIndex === this.text.length - 1) {
+        this.lessonViewed.emit();
+      } else {
+        this.currentTextIndex++;
+      }
+    } else {
+      this.lessonViewed.emit();
+    }
+  }
 }
